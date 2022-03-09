2f3a0007a5813f3f3f315a8a117769f117df6ec9
M V Ganesh Kumar <contact@mvganeshkumar.com>
2022-03-09 14:38:18 +0530

    updated index

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ const cors = require('cors');
 const connectToDatabase = require('./database/connection');
 const authRoutes = require('./routes/auth');
 const studentsRoutes = require('./routes/students');
-
+const collegeRoutes = require('./routes/colleges');
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+	cors({
+		origin: '*',
+		credentials: true,
+		optionsSuccessStatus: 200,
+	}),
+);
 
 connectToDatabase();
 
 app.use('/auth', authRoutes);
 app.use('/students', studentsRoutes);
+app.use('/colleges', collegeRoutes);
 
 app.get('/', (req, res) => {
 	res.send('Tech Maestros API is running...');
