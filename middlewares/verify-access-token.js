0e810c81a109de98588dd41fd590e7b554d9671d
M V Ganesh Kumar <contact@mvganeshkumar.com>
2022-03-09 11:59:21 +0530

    updated middleware

--- middlewares/verify-access-token.js
+++ middlewares/verify-access-token.js
@@ -1,19 +1,18 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 const verifyAccessToken = (req, res, next) => {
-    const authHeader = req.headers["authorization"];
-    if (authHeader) {
-        jwt.verify(authHeader, process.env.SERVER_SECRET, (error, user) => {
-            if (error) {
-                res.status(403).json({ errorMessage: "Access token is invalid" });
-            }
-            req.user = user;
-            next();
-        });
-    }
-    else {
-        res.status(401).json({ errorMessage: "No access token provided" });
-    }
+	const authHeader = req.headers['authorization'];
+	if (authHeader) {
+		jwt.verify(authHeader, process.env.SERVER_SECRET, (error, user) => {
+			if (error) {
+				res.status(403).json({ errorMessage: 'Access token is invalid' });
+			}
+			req.user = user;
+			next();
+		});
+	} else {
+		res.status(401).json({ errorMessage: 'No access token provided' });
+	}
 };
 
 module.exports = verifyAccessToken;
