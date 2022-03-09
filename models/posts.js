36767b3815586f1898ff41f7e2cf2664669c899f
Akaash01 <54339987+Akaash01@users.noreply.github.com>
2022-03-09 19:27:11 +0530

    updated model

---
+++ models/posts.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const postsSchema = new Schema({
+	title: {
+		type: String,
+		required: [true, 'post name is required'],
+	},
+	description: {
+		introduction: {
+			type: String,
+			required: [true, 'post intro is required'],
+		},
+		type: {
+			type: String,
+			required: [true, 'post type is required'],
+		},
+		locations: [
+			{
+				type: String,
+				required: [true, 'post locations is required'],
+			},
+		],
+		skills: [
+			{
+				type: String,
+				required: [true, 'post skills is required'],
+			},
+		],
+		deadline: {
+			type: Date,
+		},
+	},
+	company: {
+		type: Schema.Types.ObjectId,
+		ref: 'companies',
+	},
+	requirements: [
+		{
+			type: String,
+			required: [true, 'post requirements is required'],
+		},
+	],
+	pay: {
+		type: Number,
+		required: [true, 'post pay is required'],
+	},
+	colleges: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'colleges',
+		},
+	],
+	registeredStudents: {
+		type: Schema.Types.ObjectId,
+		ref: 'students',
+	},
+});
+
+const posts = model('posts', postsSchema);
+
+module.exports = posts;
