ca23822e999e3f7779cf258e5f5bb7fce1499824
M V Ganesh Kumar <contact@mvganeshkumar.com>
2022-03-09 16:40:56 +0530

    updated route

--- routes/auth.js
+++ routes/auth.js
@@ -5,40 +5,26 @@ const Colleges = require('../models/colleges');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const registerStudent = async (req, res, Students) => {
+const registerUser = async (req, res) => {
 	try {
-		const { registrationNumber } = req.body;
-		const isStudentExisting = await Students.findOne({
-			registrationNumber: registrationNumber,
-		});
-		if (isStudentExisting) {
+		const userType = req.body.userType;
+		let Model, email, existingUser;
+		if (userType === 'student') {
+			Model = Students;
+			email = req.body.collegeEmail;
+			existingUser = await Model.findOne({ collegeEmail: email });
+		} else if (userType === 'college') {
+			Model = Colleges;
+			email = req.body.email;
+			existingUser = await Model.findOne({ email: email });
+		}
+		if (existingUser) {
 			return res.status(409).json({
-				errorMessage:
-					'Registeration number already exists, please try a different registeration number',
+				errorMessage: 'Email already exists, please try a different email',
 			});
 		}
 		req.body.password = await bcrypt.hash(req.body.password, 10);
-		const student = await new Students(req.body);
-		await student.save();
-		res.status(201).json(student.id);
-	} catch (error) {
-		res.status(500).json({ errorMessage: error.message });
-	}
-};
-
-const registerCollege = async (req, res, Colleges) => {
-	try {
-		const { code } = req.body;
-		const isUserExisting = await Colleges.findOne({
-			code: code,
-		});
-		if (isUserExisting) {
-			return res.status(409).json({
-				errorMessage: 'College code already exists, please try a different college code',
-			});
-		}
-		req.body.password = await bcrypt.hash(req.body.password, 10);
-		const user = await new Colleges(req.body);
+		const user = await new Model(req.body);
 		await user.save();
 		res.status(201).json(user.id);
 	} catch (error) {
@@ -48,48 +34,32 @@ const registerCollege = async (req, res, Colleges) => {
 
 router.post('/register', async (req, res) => {
 	try {
-		const { user } = req.body;
-		if (user === 'student') {
-			registerStudent(req, res, Students);
-		} else if (user == 'college') {
-			registerCollege(req, res, Colleges);
-		}
+		registerUser(req, res);
 	} catch (error) {
 		res.status(500).json({ errorMessage: error.message });
 	}
 });
 
-const loginStudent = async (req, res, Students) => {
-	const { registrationNumber, password } = req.body;
-	const student = await Students.findOne({ registrationNumber: registrationNumber });
-	if (!student) {
-		return res
-			.status(401)
-			.json({ errorMessage: 'Wrong registeration number, please try again' });
+const loginUser = async (req, res) => {
+	const userType = req.body.userType,
+		password = req.body.password;
+	let Model, email, existingUser;
+	if (userType === 'student') {
+		Model = Students;
+		email = req.body.collegeEmail;
+		existingUser = await Model.findOne({ collegeEmail: email });
+	} else if (userType === 'college') {
+		Model = Colleges;
+		email = req.body.email;
+		existingUser = await Model.findOne({ email: email });
 	}
-	const isAuthenticated = await bcrypt.compare(password, student.password);
+	if (!existingUser) {
+		return res.status(401).json({ errorMessage: 'Wrong email, please try again' });
+	}
+	const isAuthenticated = await bcrypt.compare(password, existingUser.password);
 	if (isAuthenticated) {
 		const accessToken = jwt.sign(
-			{ id: student._id, name: student.name },
-			process.env.SERVER_SECRET,
-		);
-		return res.json({ accessToken });
-	}
-	res.status(401).json({ errorMessage: 'Wrong password, please try again' });
-};
-
-const loginCollege = async (req, res, Colleges) => {
-	const { code, password } = req.body;
-	const college = await Colleges.findOne({ code: code });
-	if (!college) {
-		return res
-			.status(401)
-			.json({ errorMessage: 'Wrong registeration number, please try again' });
-	}
-	const isAuthenticated = await bcrypt.compare(password, college.password);
-	if (isAuthenticated) {
-		const accessToken = jwt.sign(
-			{ id: college._id, name: college.name },
+			{ id: existingUser._id, name: existingUser.name },
 			process.env.SERVER_SECRET,
 		);
 		return res.json({ accessToken });
@@ -99,12 +69,7 @@ const loginCollege = async (req, res, Colleges) => {
 
 router.post('/login', async (req, res) => {
 	try {
-		const { user } = req.body;
-		if (user === 'student') {
-			loginStudent(req, res, Students);
-		} else if (user === 'college') {
-			loginCollege(req, res, Colleges);
-		}
+		loginUser(req, res);
 	} catch (error) {
 		res.status(500).json({ errorMessage: error.message });
 	}
