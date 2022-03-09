08a2cfac6b1e901fb2458aca715e4374eaceff51
unknown <nivimala210@gmail.com>
2022-03-09 12:59:40 +0530

    updated model

--- models/colleges.js
+++ models/colleges.js
@@ -6,6 +6,77 @@ const collegeSchema = new Schema({
 		type: String,
 		required: [true, 'Name is required'],
 	},
+	code: {
+		type: Number,
+		required: [true, 'Code is required'],
+	},
+	description: {
+		type: String,
+	},
+	isVerified: {
+		type: Boolean,
+	},
+	contact: {
+		email: {
+			type: String,
+			required: [true, 'Email is required'],
+		},
+		website: {
+			type: String,
+		},
+		logo: {
+			type: String,
+		},
+		address: {
+			street: {
+				type: String,
+			},
+			city: {
+				type: String,
+			},
+			state: {
+				type: String,
+			},
+		},
+	},
+	stats: {
+		departments: [
+			{
+				name: {
+					type: String,
+				},
+				description: {
+					type: String,
+				},
+				hodName: {
+					type: String,
+				},
+				totalFaculties: {
+					type: Number,
+				},
+			},
+		],
+		totalStudents: {
+			type: Number,
+		},
+		totalStudentsPlaced: {
+			type: Number,
+		},
+		package: {
+			highest: {
+				type: Number,
+			},
+			average: {
+				type: Number,
+			},
+		},
+	},
+	companies: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'companies',
+		},
+	],
 });
 
 const colleges = model('colleges', collegeSchema);
